1cc9d777aae21d097d0b77aae63750911e67459d
Lisa Lee <snickers495@gmail.com>
2018-02-07 04:05:52 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
-// Code MovieReviews Here
+// Code MovieReviews Here// Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews = ({props}) => {
+    return(
+      <div className="review-list">
+      {props.reviews.map(review => {
+        return(
+          <div className="review">
+          review
+          </div>
+        )
+      })}
+      </div>
+    )
+}
+MovieReviews.defaultProp = {
+  reviews: 'reviews'
+}
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,22 @@
 // Code SearchableMovieReviewsContainer Here
+
+// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+
+export default class SearchableMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: [],
+      searchTerm: {}
+    }
+  }
+  render(){
+    return(
+      <div className="searchable-movie-reviews">
+
+      </div>
+    )
+  }
+
+}
